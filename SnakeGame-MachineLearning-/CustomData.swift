089ce575d93f984cd69a6913c83ae6d08d826510
Xometrics <109303454+Xometrics@users.noreply.github.com>
2021-04-20 12:19:13 +0000

    missing file added

--- SnakeGame-MachineLearning-/CustomData.swift
+++ SnakeGame-MachineLearning-/CustomData.swift
@@ -2,4 +2,10 @@
 //  CustomData.swift
 //  SnakeGame-MachineLearning-
 //
-//  Created by Edward O'Neill on 12/9/
+//  Created by Edward O'Neill on 12/9/19.
+//  Copyright © 2019 Edward O'Neill. All rights reserved.
+//
+
+import UIKit
+
+struct Cust
