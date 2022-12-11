484d5f1a9b411fcb0b1d5658be2d4badc3da7973
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-11 10:17:45 +0000

    Release 3.5.1

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -3,4 +3,11 @@
 //  SnakeGame-MachineLearning-
 //
 //  Created by Edward O'Neill on 12/9/19.
-//  Copyright © 2019 Edward O'Neill. All rights
+//  Copyright © 2019 Edward O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var wind
