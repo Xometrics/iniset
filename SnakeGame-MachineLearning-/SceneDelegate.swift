1dd1ee303c732a81c0d1cdb972682e3a1cf49f24
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-11 08:23:52 +0000

    some cleaning

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -1,3 +1,6 @@
 //
 //  SceneDelegate.swift
-//  SnakeGame-MachineLearni
+//  SnakeGame-MachineLearning-
+//
+//  Created by Edward O'Neill on 12/9/19.
+//  Copyright © 2019 Edward O'Neill. All rights
