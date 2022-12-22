6762c30b3f4d1cd587f7d894f6a1d280ccfda474
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-22 08:42:22 +0000

    Update output.

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -22,4 +22,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the 
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associ
