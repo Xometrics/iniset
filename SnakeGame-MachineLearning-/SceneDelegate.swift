be0460fa9d7e9560fd1cfe791c6348a3704c3159
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-23 22:39:16 +0000

    whitespace removed

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -24,4 +24,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scen
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:
