e9fd0e249c1bf36d9c5ed23b7b704ae35b4b4f4f
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-24 20:42:09 +0000

    fixing lint

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -28,4 +28,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-   
+        // Called when the scene has moved from an inactive state to an active 
