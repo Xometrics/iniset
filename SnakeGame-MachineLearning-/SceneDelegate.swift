e99a3c46bd431b149baa7729dc7a2bd7285139d8
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-25 20:12:15 +0000

    misc commit

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -32,4 +32,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
-    func sceneWillResignActive(_ scene: UIScen
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+  
