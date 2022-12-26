611fcc8e7126a028d7e8a4beb4a7bea988d153dd
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-26 21:46:45 +0000

    spacing cleanup

--- SnakeGame-MachineLearning-/SceneDelegate.swift
+++ SnakeGame-MachineLearning-/SceneDelegate.swift
@@ -38,4 +38,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene tra
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the chang
