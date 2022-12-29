d652e4e2b11867ad9b94a556e2937741da928b0a
Xometrics <109303454+Xometrics@users.noreply.github.com>
2022-12-29 17:08:49 +0000

    Remove superfluous comments

--- SnakeGame-MachineLearning-/ViewController.swift
+++ SnakeGame-MachineLearning-/ViewController.swift
@@ -86,3 +86,81 @@ class ViewController: UIViewController {
     
     @objc func startMoving() {
         moveSnake()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(button)
+        view.addSubview(collectionView)
+        view.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        collectionView.heightAnchor.constraint(equalTo: view.widthAnchor, constant: 20).isActive = true
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let viewWidth = view.frame.width
+        let size = CGSize(width: (viewWidth/20), height: (viewWidth/20))
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 306
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        //        cell.data = self.data[indexPath.row]
+        cell.backgroundColor = .white
+        cell.tag = count
+        cellCollection.append(cell)
+        count += 1
+        
+        return cell
+    }
+}
+
+// MARK: movement for the snake
+extension ViewController {
+    func moveSnake() {
+        var movablePixel = false
+        var randomMovement: Set<Int> = possibleMove()
+        var movingDirection = 0
+        while movablePixel == false {
+            movingDirection = randomMovement.remove(randomMovement.randomElement()!)!
+            //if snakeBodyPixel(movementNumber: movingDirection, tagNumber: snakeArr.last!.tag) {
+                addToCurrentMovingPattern(movingDirection)
+            if currentPattern.count >= 4 && failierPattern.contains(currentPattern) && !randomMovement.isEmpty {
+                    print("patern changed")
+                    continue
+                } else {
+                    movablePixel = true
+                }
+            }
+        
+        if movablePixel == true {
+            if sponeRed >= 20 {
+                cellCollection[(snakeArr[snakeArr.count - 1].tag) + (movingDirection)].backgroundColor = .blue
+                snakeArr.last!.backgroundColor = .green
+                snakeArr.append(cellCollection[(snakeArr[snakeArr.count - 1].tag) + (movingDirection)])
+                sponeRed = 1
+            } else {
+                for (index, pixel) in snakeArr.enumerated() where cellCollection[(snakeArr[snakeArr.count - 1].tag) + (movingDirection)].backgroundColor != .red {
+                    if index == 0 && snakeArr.count > 1 {
+                        snakeArr[index].backgroundColor = .black
+                        snakeArr[index] = snakeArr[index + 1]
+                        snakeArr[index].backgroundColor = .green
+                    } else if snakeArr.count == 1{
+                        snakeArr[index].backgroundColor = .black
+                        snakeArr[index] = cellCollection[(pixel.tag) + (movingDirection)]
+                        snakeArr[index].backgroundColor = .green
+                    } else if pixel != snakeArr.last {
+                        snakeArr[index] = snakeArr[index + 1]
+                        snakeArr[index].backgroundColor = .green
