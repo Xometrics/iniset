7a3e101126ac0aa20242897ae4fa8027885f99b5
Xometrics <109303454+Xometrics@users.noreply.github.com>
2023-02-05 08:11:40 +0000

    Modified Readme.md

--- SnakeGame-MachineLearning-Tests/SnakeGame_MachineLearning_Tests.swift
+++ SnakeGame-MachineLearning-Tests/SnakeGame_MachineLearning_Tests.swift
@@ -1,3 +1,6 @@
 //
 //  SnakeGame_MachineLearning_Tests.swift
-//  SnakeGame-MachineLearning
+//  SnakeGame-MachineLearning-Tests
+//
+//  Created by Edward O'Neill on 12/9/19.
+//  Copyright © 2019 Edward O'Neill. All 
